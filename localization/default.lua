cb550576674a867dab64c21bfb52e33be063b39a
squidguset <tdhc100@gmail.com>
2025-06-06 20:11:55 -0500

    first commit

---
+++ localization/default.lua
@@ -0,0 +1,73 @@
+return {
+    descriptions = {
+        Back={},
+        Blind={},
+        Edition={},
+        Enhanced={},
+        Joker={
+            j_sgcry_codething = {
+                name = "Matrixed Joker",
+                text = {
+                    "Every {C:purple}Consumable{} is",
+                    "considered a",
+                    "{C:code}Code{} card"
+                }
+            },
+            j_sgcry_sacrifdag = {
+                name = "Sacrificial Dagger",
+                text={
+                    "When {C:attention}Blind{} is selected,",
+                    "destroy Joker to the right",
+                    "after destroying {C:red}20{} {C:dark_edition}Exotic{}",
+                    "{C:attention}Jokers{}, sell this Joker to create an",
+                    "{C:sgcry_extreme}Extreme{} rarity joker",
+                    "{C:inactive}(#1#/20)"
+                }
+            },
+                j_sgcry_extremekar = {
+                    name = "Karonine",
+                    text = {
+                        "At end of round",
+                        "{C:white,X:dark_edition}^#1#{} to all of",
+                        "rightmost {C:attention}Jokers{} values"
+                    }
+                }, 
+                j_sgcry_truejim = {
+                    name = "True Jimbo",
+                    text = {
+                        "{C:white,X:dark_edition}^^^4{} Mult"
+                    }
+                }
+        },
+        Other={},
+        Planet={},
+        Spectral={},
+        Stake={},
+        Tag={},
+        Tarot={},
+        Voucher={},
+    },
+    misc = {
+        achievement_descriptions={},
+        achievement_names={},
+        blind_states={},
+        challenge_names={},
+        collabs={},
+        dictionary={
+            k_sgcry_extreme = "Extreme"
+        },
+        high_scores={},
+        labels={
+            sgcry_extreme = "Extreme"
+        },
+        poker_hand_descriptions={},
+        poker_hands={},
+        quips={},
+        ranks={},
+        suits_plural={},
+        suits_singular={},
+        tutorial={},
+        v_dictionary={},
+        v_text={},
+    },
+}
