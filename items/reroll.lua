acddb90e1a96df6cfd2ec7aa4cfaeaca75778f30
squidguset <tdhc100@gmail.com>
2025-06-19 15:06:54 -0500

    TOGGLEABLE REROLL

--- items/reroll.lua
+++ items/reroll.lua
@@ -8,13 +8,14 @@ end)
 
 local rrsr = G.FUNCS.reroll_shop
 function  G.FUNCS:reroll_shop(e)
-    
+    if G.GAME.sgcrydorerolls then
     if G.GAME.sgcryrerollleft > 0 then
     G.GAME.sgcryrerollleft = G.GAME.sgcryrerollleft - 1
     else
         change_shop_size(-1)
         G.GAME.sgcryrerollcost = G.GAME.sgcryrerollcost + 1
     end
+end
     local ret = rrsr(e)
     
     return ret
@@ -27,4 +28,14 @@ function  G.FUNCS:toggle_shop(e)
     change_shop_size(G.GAME.sgcryrerollcost)
     G.GAME.sgcryrerollcost = 0
     return ret
+end
+
+
+function SGCRY:REROLLBUTTON ()
+    if G.GAME.sgcrydorerolls then
+    return {n=G.UIT.R, config={align = "cm", maxw = 1.3, minw = 1}, nodes={
+                              {n=G.UIT.T, config={ref_table = G.GAME, ref_value = 'sgcryrerollleft', scale = 0.75, colour = G.C.WHITE, shadow = true}},
+                              {n=G.UIT.T, config={text = " left", scale = 0.7, colour = G.C.WHITE, shadow = true}},
+                            }}
+                        end
 end
--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 Cryptid.mod_whitelist["CMOD"] = true
 
+
 assert(SMODS.load_file("items/editions.lua"))()
 assert(SMODS.load_file("items/jokers.lua"))()
 assert(SMODS.load_file("items/extreme.lua"))()
@@ -7,7 +8,33 @@ assert(SMODS.load_file("items/misc.lua"))()
 assert(SMODS.load_file("items/reroll.lua"))()
 assert(SMODS.load_file("items/vouchers.lua"))()
 
+SGCRY.SMCM = SMODS.current_mod
 
+sgcagg(function (rs)
+    if rs then
+        G.GAME.sgcrydorerolls = SGCRY.SMCM.config.reroll
+    end
+end)
+
+SMODS.current_mod.config_tab = function()
+	return {n = G.UIT.ROOT, config = {
+        r = 0.1
+		-- config values here, see 'Building a UI' page
+	}, nodes = {
+		{n=G.UIT.C,config = {padding = 0.1,align = "cm"},nodes = {
+            create_toggle({
+                label = "Reroll Rework",
+                ref_table = SGCRY.SMCM.config,
+                ref_value = "reroll",
+                active_colour = G.C.GREEN
+            }),
+            {n=G.UIT.R, config = {align = "cm", padding= 0}, nodes={
+          {n=G.UIT.T, config={text = "(Only applies at start of run)", scale = 0.35, colour = G.C.UI.TEXT_LIGHT}}
+            }}
+        }},
+        
+	}}
+end
 
 
 
